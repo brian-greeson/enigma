768e19ad7bbccd1c1e51d2ca319d9e864b2d0b96
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-01 11:20:56 -0700

    Pass tests for key attribute

--- lib/message.rb
+++ lib/message.rb
@@ -1,9 +1,19 @@
 class Message
-  attr_reader :text, :date
+  attr_reader :text, :key, :date
 
-  def initialize(message = nil, date = Date.today)
-      @date = date
-      @text = message
+  def initialize(message = nil, key = nil, date = nil)
+    @text = message
+    @key  = key ? key : make_key
+    @date = date ? date : todays_date
   end
+
+  def make_key
+    Random.rand(99999).to_s.rjust(5,"0")
+  end
+
+  def todays_date
+
+  end
+
 
 end
