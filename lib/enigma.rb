8c4442079657b92570ea7d275c751385c23c9622
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-01 15:36:29 -0700

    Pass encrypt test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,24 @@
 require_relative "cipher"
-
+require_relative "shift"
 class Enigma < Cipher
 
-  def encrypt
+  def encrypt(text, key, date)
+    plaintext = Message.new(text, key, date)
+    ciphertext = Message.new("", key, date)
+    offsets = Shift.new(key, plaintext.date)
 
+    plaintext.text.chars.each_slice(4) do |characters|
+     ciphertext.text << encrypt_chars(characters, offsets).join
+    end
+    
+    {encryption: ciphertext.text, key: ciphertext.key, date: ciphertext.date}
   end
 
-  # def decrypt
-  #
-  # end
+  def encrypt_chars(plaintext_chars, offsets)
+    plaintext_chars.reduce([]) do |ciphertext, character|
+      ciphertext << @character_set.shift(character, offsets.all[ciphertext.size])
+      ciphertext
+    end
+  end
+
 end
