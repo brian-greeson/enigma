a7c483a16b85c06560babacb4ae3f7471bdc1b3d
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-01 09:28:26 -0700

    Test if message can take strings or hashes

--- test/message_test.rb
+++ test/message_test.rb
@@ -7,9 +7,17 @@ class MessageTest < Minitest::Test
     assert_instance_of Message, Message.new
   end
 
-  def test_it_takes_a_string_message
-    message = Message.new("Hello World. Get ready for tacos!")
+  def test_it_can_take_a_string_message
     expected_message = "Hello World. Get ready for tacos!"
+    message = Message.new(expected_message)
+
+    assert_equal expected_message, message.text
+  end
+
+  def test_it_can_take_a_hash_message
+    expected_message = "Hello World. Get ready for tacos!"
+    message = Message.new({text: expected_message})
+
     assert_equal expected_message, message.text
   end
 
