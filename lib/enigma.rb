59b2d4bf28728f2e7d5bf6ef053bfbe84d957995
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-01 10:35:21 -0700

    Add enigma class and test files

---
+++ lib/enigma.rb
@@ -0,0 +1,12 @@
+require_relative "cipher"
+
+class Enigma < Cipher
+
+  def encrypt
+
+  end
+
+  # def decrypt
+  #
+  # end
+end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,4 +3,29 @@ require "./lib/enigma"
 
 class EnigmaTest < Minitest::Test
 
+  def test_it_exists
+    assert_instance_of Enigma, Enigma.new
+  end
+
+  def test_it_can_encypt_a_message
+    enigma = Enigma.new
+    expected = {
+                encryption: "keder ohulw",
+                key: "02715",
+                date: "040895"
+               }
+
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  # def test_it_can_decrypt_a_message
+  #   enigma = Enigma.new
+  #   expected = {
+  #               decryption: "hello world",
+  #               key: "02715",
+  #               date: "040895"
+  #              }
+  #   assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+  # end
+
 end
