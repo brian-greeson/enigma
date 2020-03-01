8848596f59ee101d18aa15abdf7dc7d68f7e0654
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-01 09:43:11 -0700

    Pass message date tests

--- lib/message.rb
+++ lib/message.rb
@@ -1,13 +1,14 @@
 class Message
-  attr_reader :message
+  attr_reader :text, :date
 
-  def initialize(message = nil)
-    @message_text= message if message.is_a?(String)
-    @message_text = message[:text] if message.is_a?(Hash)
-  end
-
-  def text
-    @message_text
+  def initialize(message = nil, date = Date.today)
+    if message.is_a?(Hash)
+      @text = message[:text]
+      @date = message.has_key?(:date) ? message[:date] : date
+    else
+      @date = date
+      @text = message
+    end
   end
 
 end
