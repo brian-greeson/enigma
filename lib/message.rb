7773a6874962e632b89fc2c0ccc4e85adddab760
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-01 09:28:54 -0700

    Pass can_take_a_hash_message and can_take_a_string_message

--- lib/message.rb
+++ lib/message.rb
@@ -2,11 +2,12 @@ class Message
   attr_reader :message
 
   def initialize(message = nil)
-    @message = message
+    @message_text= message if message.is_a?(String)
+    @message_text = message[:text] if message.is_a?(Hash)
   end
 
   def text
-    @message
+    @message_text
   end
 
 end
