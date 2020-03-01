a9e714ea1ecec0913c0781dee76165ca6b6c9605
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-01 10:30:35 -0700

    Remove accepting hash as message argument

--- lib/message.rb
+++ lib/message.rb
@@ -2,13 +2,8 @@ class Message
   attr_reader :text, :date
 
   def initialize(message = nil, date = Date.today)
-    if message.is_a?(Hash)
-      @text = message[:text]
-      @date = message.has_key?(:date) ? message[:date] : date
-    else
       @date = date
       @text = message
-    end
   end
 
 end
--- test/message_test.rb
+++ test/message_test.rb
@@ -7,36 +7,25 @@ class MessageTest < Minitest::Test
     assert_instance_of Message, Message.new
   end
 
-  def test_it_can_take_a_string_message
+  def test_it_knows_message_text
     expected_message = "Hello World. Get ready for tacos!"
     message = Message.new(expected_message)
 
     assert_equal expected_message, message.text
   end
 
-  def test_it_can_take_a_hash_message
-    expected_message = "Hello World. Get ready for tacos!"
-    message = Message.new({text: expected_message})
-
-    assert_equal expected_message, message.text
-  end
 
   def test_it_knows_date_of_message
     expected_message = "Hello World. Get ready for tacos!"
     message1 = Message.new(expected_message)
-    message2 = Message.new({text: expected_message})
 
     assert_equal Date.today, message1.date
-    assert_equal Date.today, message2.date
 
     expected_date = Date.new(1999,12,13)
-    message1 = Message.new(expected_message, expected_date)
-    message2 = Message.new({text: expected_message, date: expected_date})
-    
+    message2 = Message.new(expected_message, expected_date)
 
-    assert_equal expected_date, message1.date
     assert_equal expected_date, message2.date
-  end
 
+  end
 
 end
