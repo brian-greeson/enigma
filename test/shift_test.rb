857a86b8db3944f81722e87cb9e28c5eae1d9431
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-01 13:34:30 -0700

    Add shift tests

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -4,7 +4,17 @@ require "./lib/shift"
 class ShiftTest < Minitest::Test
 
   def test_it_exists
-    assert_instance_of Shift, Shift.new
+    key, date = mock
+    assert_instance_of Shift, Shift.new("01234", "011399")
+  end
+
+  def test_it_can_generate_key_offsets
+    shift = Shift.new("02715", "040895")
+
+    assert_equal  3, shift.offset_a
+    assert_equal 27, shift.offset_b
+    assert_equal 73, shift.offset_c
+    assert_equal 20, shift.offset_d
   end
 
 end
