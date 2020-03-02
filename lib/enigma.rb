c375abf2087c1ae94a1c18349dd9254550fb1488
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-01 21:04:52 -0700

    Add transcode methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,24 +1,43 @@
 require_relative "cipher"
 require_relative "shift"
+
 class Enigma < Cipher
 
   def encrypt(text, key, date)
-    plaintext = Message.new(text, key, date)
-    ciphertext = Message.new("", key, date)
-    offsets = Shift.new(key, plaintext.date)
-
-    plaintext.text.chars.each_slice(4) do |characters|
-     ciphertext.text << encrypt_chars(characters, offsets).join
-    end
-    
-    {encryption: ciphertext.text, key: ciphertext.key, date: ciphertext.date}
+    encrypted_message = transcode_message(Message.new(text, key, date), :encrypt)
+    {
+      encryption: encrypted_message.text,
+      key: encrypted_message.key,
+      date: encrypted_message.date
+    }
   end
 
-  def encrypt_chars(plaintext_chars, offsets)
-    plaintext_chars.reduce([]) do |ciphertext, character|
-      ciphertext << @character_set.shift(character, offsets.all[ciphertext.size])
-      ciphertext
+  def decrypt(text, key, date)
+    decrypted_message = transcode_message(Message.new(text, key, date), :decrypt)
+    {
+      decryption: decrypted_message.text,
+      key: decrypted_message.key,
+      date: decrypted_message.date
+    }
+  end
+
+  def transcode_message(message, direction)
+    offsets = Shift.new(message.key, message.date, direction)
+    transcoded_message = Message.new("", message.key, message.date)
+
+    message.text.chars.each_slice(offsets.all.size) do |characters|
+     transcoded_message.text << transcode_chars(characters, offsets).join
+    end
+
+    transcoded_message
+  end
+
+  def transcode_chars(characters, offsets)
+    characters.reduce([]) do |shifted_chars, character|
+      shifted_chars << @character_set.shift(character, offsets.all[shifted_chars.size])
+      shifted_chars
     end
   end
+
 
 end
