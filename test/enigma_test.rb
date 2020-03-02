15a9bbdef97f70ef4ee0487a6338e97ed9d8beb8
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-01 21:03:45 -0700

    Test transcode methods instead of encrypt/decrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,7 +7,7 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, Enigma.new
   end
 
-  def test_it_can_encypt_a_message
+  def test_it_can_encrypt_a_message
     enigma = Enigma.new
     expected = {
                 encryption: "keder ohulw",
@@ -18,24 +18,34 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
   end
 
-  def test_it_can_shift_chars
+  def test_it_can_decrypt_a_message
+    enigma = Enigma.new
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
+  def test_it_can_transcode_chars
     enigma = Enigma.new
     offsets = mock
     offsets.stubs(:all).returns([1,2,3,4])
 
-    assert_equal ["b","d","f","h"], enigma.encrypt_chars(["a","b","c","d"], offsets)
-    assert_equal ["y"," ","b"], enigma.encrypt_chars(["x","y","z"], offsets)
-
+    assert_equal ["b","d","f","h"], enigma.transcode_chars(["a","b","c","d"], offsets)
+    assert_equal ["y"," ","b"], enigma.transcode_chars(["x","y","z"], offsets)
   end
 
-  # def test_it_can_decrypt_a_message
-  #   enigma = Enigma.new
-  #   expected = {
-  #               decryption: "hello world",
-  #               key: "02715",
-  #               date: "040895"
-  #              }
-  #   assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
-  # end
+  def test_it_can_transcode_messages
+    enigma = Enigma.new
+    plaintext_message  = Message.new("hello world", "02715", "040895" )
+    ciphertext_message = Message.new("keder ohulw", "02715", "040895" )
 
+    encrypt_result =  enigma.transcode_message(plaintext_message, :encrypt)
+    assert_equal ciphertext_message.text, encrypt_result.text
+
+    decrypt_result = enigma.transcode_message(ciphertext_message, :decrypt)
+    assert_equal plaintext_message.text, decrypt_result.text
+  end
 end
