363c039de748b8a3d5dffa2598f1bcd264365826
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-02 08:34:26 -0700

    Breakup offset methods into individual methods and tests

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,19 +1,23 @@
 class Shift
 
-    def initialize(key, date, direction = :encrypt, set_size = nil)
-      set_size = CharacterSet.new.default_set.size if !set_size
-      date_squared = date.to_i ** 2
-
+    def initialize(key, date, direction = :encrypt)
+      @key  = key
+      @date = date
       @offsets = [
-                    offset_a(key, date_squared),
-                    offset_b(key, date_squared),
-                    offset_c(key, date_squared),
-                    offset_d(key, date_squared)
+                    shift_a,
+                    shift_b,
+                    shift_c,
+                    shift_d
                   ]
       set_direction(direction)
+      # All theses methods and typing, just to do this...
       # 4.times do |i|
       #   @offsets << key[i..i+1].to_i + date_squared / 10 ** (3 - i) % 10
       # end
+    end
+
+    def date_squared
+       @date_squared ||= @date.to_i ** 2
     end
 
     def set_direction(direction)
@@ -24,22 +28,50 @@ class Shift
       @offsets
     end
 
-    def offset_a(key, date_squared)
-      key[0..1].to_i + date_squared / 1000 % 10
-
+    def shift_a
+      key_a + date_offset_a
     end
 
-    def offset_b(key, date_squared)
-      key[1..2].to_i + date_squared / 100 % 10
-
+    def shift_b
+      key_b + date_offset_b
     end
 
-    def offset_c(key, date_squared)
-      key[2..3].to_i + date_squared / 10 % 10
+    def shift_c
+      key_c + date_offset_c
     end
 
-    def offset_d(key, date_squared)
-      key[3..4].to_i + date_squared % 10
+    def shift_d
+      key_d + date_offset_d
+    end
+
+    def date_offset_a
+       date_squared / 1000 % 10
+    end
+
+    def date_offset_b
+      date_squared / 100 % 10
+    end
+
+    def date_offset_c
+      date_squared / 10 % 10
+    end
+
+    def date_offset_d
+      date_squared % 10
+    end
+
+    def key_a
+      @key[0..1].to_i
+    end
+
+    def key_b
+      @key[1..2].to_i
+    end
+    def key_c
+      @key[2..3].to_i
+    end
+    def key_d
+      @key[3..4].to_i
     end
 
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -9,12 +9,11 @@ class ShiftTest < Minitest::Test
 
   def test_it_can_generate_key_offsets
     shift = Shift.new("02715", "040895")
-    date_squared = "040895".to_i ** 2
-    key = "02715"
-    assert_equal  3, shift.offset_a(key, date_squared)
-    assert_equal 27, shift.offset_b(key, date_squared)
-    assert_equal 73, shift.offset_c(key, date_squared)
-    assert_equal 20, shift.offset_d(key, date_squared)
+
+    assert_equal  3, shift.shift_a
+    assert_equal 27, shift.shift_b
+    assert_equal 73, shift.shift_c
+    assert_equal 20, shift.shift_d
   end
 
   def test_it_can_give_all_the_offsets_for_encryption
@@ -27,6 +26,29 @@ class ShiftTest < Minitest::Test
     shift = Shift.new("02715", "040895", :decrypt)
 
     assert_equal [-3,-27,-73,-20], shift.all
+  end
 
+  def test_it_calcs_date_seed
+    shift = Shift.new("02715", "040895")
+
+    assert_equal 1672401025, shift.date_squared
+  end
+
+  def test_it_knows_keys_a_d
+    shift = Shift.new("02715", "040895")
+
+    assert_equal 2, shift.key_a
+    assert_equal 27, shift.key_b
+    assert_equal 71, shift.key_c
+    assert_equal 15, shift.key_d
+  end
+
+  def test_it_knows_date_offsets_a_d
+    shift = Shift.new("02715", "040895")
+
+    assert_equal 1, shift.date_offset_a
+    assert_equal 0, shift.date_offset_b
+    assert_equal 2, shift.date_offset_c
+    assert_equal 5, shift.date_offset_d
   end
 end
