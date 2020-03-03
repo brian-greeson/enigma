f4cbe513704d641ce0377a6449ee84ce1f0224de
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-02 18:10:02 -0700

    Add transcode_file behavior, remove seperate encrypt/decrypt methods

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -2,23 +2,18 @@ require_relative "character_set"
 require_relative "message"
 
 class Cipher
+
   attr_reader :character_set
+
   def initialize()
     @character_set = CharacterSet.new
   end
 
-  def encrypt_file(plaintext_file, target_file)
-    plaintext_file = File.open(plaintext_file, "r")
-    encrypted = encrypt(plaintext_file.read)
-    write_file(target_file, encrypted[:encryption])
-    encrypted
-  end
-
-  def decrypt_file(ciphertext_file, target_file, key, date)
-    ciphertext_file = File.open(ciphertext_file, "r")
-    decrypted = decrypt(ciphertext_file.read, key, date)
-    write_file(target_file, decrypted[:decryption])
-    decrypted
+  def transcode_file(files = {}, message_details = {}, direction)
+    source_text = File.open(files[:source], "r")
+    message = Message.new(source_text.read, message_details[:key], message_details[:date])
+    write_file(files[:target], transcode_message(message, direction).text)
+    message
   end
 
   def write_file(target_file, content)
@@ -27,13 +22,4 @@ class Cipher
     ciphertext_file.close
   end
 
-  
 end
-
-
-# def transcode_file(files = {}, encoding_details = {}, direction)
-#   source_text = File.open(source_file, "r")
-#   message = Message.new(source_text.read, encoding_details[:key], encoding_details[:date])
-#
-#   transcode_message(message, direction).text
-# end
